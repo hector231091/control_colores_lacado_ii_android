42a9411324669af848bbcda83b5be511eb5349aa
David Aguiar <siempredelao@gmail.com>
2021-03-13 20:54:50 +0100

    Limpieza de código y ficheros

--- app/src/main/java/com/hectormorales/colores_cabina_lacado_ii_android/MainActivity.kt
+++ app/src/main/java/com/hectormorales/colores_cabina_lacado_ii_android/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.hectormorales.colores_cabina_lacado_ii_android.databinding.ActivityMainBinding
 import com.hectormorales.colores_cabina_lacado_ii_android.view.HistoricalItem
 import com.hectormorales.colores_cabina_lacado_ii_android.view.HistoricalUiModel
-import com.opencsv.CSVParserBuilder
 import com.opencsv.CSVReader
-import com.opencsv.CSVReaderBuilder
 import java.io.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -20,9 +18,9 @@ import java.util.function.Consumer
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityMainBinding
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
@@ -60,15 +58,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.registerContinueButton.setOnClickListener {
-            register_and_continue()
+            onRegisterAndContinueButtonClick()
         }
 
-        binding.registerStopButton.setOnClickListener() {
-            register_and_stop()
+        binding.registerStopButton.setOnClickListener {
+            onRegisterAndStopButtonClick()
         }
 
-        binding.registerEndButton.setOnClickListener() {
-            register_and_end()
+        binding.registerEndButton.setOnClickListener {
+            onRegisterAndEndButtonClick()
         }
 
         // Esto es temporal, la idea es mostrar algo para ver que todo funciona bien, pero
@@ -79,54 +77,54 @@ class MainActivity : AppCompatActivity() {
             HistoricalItem("fakeID", "40100100", "20:06:33", "20:06:33", "20:06:33", 2, ""),
             HistoricalItem("fakeID", "40100100", "20:06:33", "20:06:33", "20:06:33", 2, "")
         )
-        show_data(historicalItems)
+        showData(historicalItems)
     }
 
-    private fun show_data(historicalItems: List<HistoricalItem>) {
+    private fun showData(historicalItems: List<HistoricalItem>) {
         val historicalUiModel = HistoricalUiModel(historicalItems)
         binding.historicalView.bind(historicalUiModel)
     }
 
-    private fun check_all_necessary_input_data(): Boolean {
-        // Variable de ayuda.
-        var variable = ""
-
+    private fun checkAllNecessaryInputData(): Boolean {
         // Comprobamos que todos los campos están rellenos.
-        if (check_colour() && check_hours() && check_hangers()) {
-            variable = ""
+        if (checkColour() && checkHours() && checkHangers()) {
+            return true
         } else {
             //errores.text = "No furula"
-            if (check_colour() == false) {
-                //Snackbar
-                //val message = "El color no es correcto."
-                //Snackbar.make(findViewById(R.id.binding.colourEntry), message, Snackbar.LENGTH_LONG).show()
+            when {
+                !checkColour() -> {
+                    //Snackbar
+                    //val message = "El color no es correcto."
+                    //Snackbar.make(findViewById(R.id.binding.colourEntry), message, Snackbar.LENGTH_LONG).show()
 
-                // Hacemos un "AlertDialog".
-                val message = "El color introducido no es correcto."
-                dialog_alert(message)
+                    // Hacemos un "AlertDialog".
+                    val message = "El color introducido no es correcto."
+                    showErrorDialog(message)
 
-            } else if (check_hours() == false) {
-                // Hacemos un "AlertDialog".
-                val message = "Debes introducir las tres horas."
-                dialog_alert(message)
+                }
+                !checkHours() -> {
+                    // Hacemos un "AlertDialog".
+                    val message = "Debes introducir las tres horas."
+                    showErrorDialog(message)
 
-            } else if (check_hangers() == false) {
-                // Hacemos un "AlertDialog".
-                val message = "El campo de bastidores debe estar relleno."
-                dialog_alert(message)
+                }
+                !checkHangers() -> {
+                    // Hacemos un "AlertDialog".
+                    val message = "El campo de bastidores debe estar relleno."
+                    showErrorDialog(message)
+                }
             }
-            variable = "1"
+            return false
         }
-        return variable.isEmpty()
     }
 
-    private fun register_and_continue() {
-        if (check_all_necessary_input_data()) {
+    private fun onRegisterAndContinueButtonClick() {
+        if (checkAllNecessaryInputData()) {
             // Ponemos lo último que hemos introducido en la pantalla:
-            show_data(emptyList())
+            showData(emptyList())
 
             // Poner función para guardar esto en un archivo:
-            write_data_in_storage()
+            writeDataInStorage()
 
             // Copiamos la hora del final_color en el inicio_cambio:
             binding.colourStartTimeLabel.text = binding.colourEndTimeLabel.text
@@ -143,14 +141,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun register_and_stop() {
+    private fun onRegisterAndStopButtonClick() {
         // Poner un mensaje de que se va a registrar e irse al descanso y si se quiere continuar o no.
-        if (check_all_necessary_input_data()) {
+        if (checkAllNecessaryInputData()) {
             // Ponemos el lo último que hemos introducido en la pantalla:
-            show_data(emptyList())
+            showData(emptyList())
 
             // Poner función para guardar esto en un archivo:
-            write_data_in_storage()
+            writeDataInStorage()
 
             // Borramos todos los labels que hay que borrar:
             binding.colourEntry.setText("")
@@ -165,16 +163,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun register_and_end() {
-
+    private fun onRegisterAndEndButtonClick() {
         // Poner un mensaje que avise de que se va a cerrar la aplicación y decidir si continuar o no.
 
-        if (check_all_necessary_input_data()) {
+        if (checkAllNecessaryInputData()) {
             // Ponemos el lo último que hemos introducido en la pantalla:
-            show_data(emptyList())
+            showData(emptyList())
 
             // Poner función para guardar esto en un archivo:
-            write_data_in_storage()
+            writeDataInStorage()
 
             // Borramos todos los labels que hay que borrar:
             binding.colourEntry.setText("")
@@ -192,50 +189,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun write_data_in_storage() {
-
+    private fun writeDataInStorage() {
         // Creamos una variable String con todos los datos.
-        val data = binding.colourEntry.getText().toString() + ";" +
+        val data = binding.colourEntry.text.toString() + ";" +
                 binding.changeStartTimeLabel.text.toString() + ";" +
                 binding.colourStartTimeLabel.text.toString() + ";" +
                 binding.colourEndTimeLabel.text.toString() + ";" +
-                binding.hangersEntry.getText().toString() + ";" +
-                binding.observationsEntry.getText().toString() + "\n"
+                binding.hangersEntry.text.toString() + ";" +
+                binding.observationsEntry.text.toString() + "\n"
 
         // Dirección en la que lo vamos a guardar
-        val file_direccion = getExternalFilesDir(null)
+        val externalFilesFolderPath = getExternalFilesDir(null)
 
         // Creamos una carpeta dentro de la dirección anterior.
-        val folder = File(file_direccion, "Datos Lacado")
+        val dataFolder = File(externalFilesFolderPath, "Datos Lacado")
         //Log.i("Dirección: ", folder.toString())
         // Comprobamos que existe la carpeta. En caso contrario la creamos.
-        if (!folder.exists()) {
-            folder.mkdir()
+        if (!dataFolder.exists()) {
+            dataFolder.mkdir()
         }
         // Creamos el fichero y le pasamos los datos que debe almacenar.
-        val ficheroFisico = File(folder, "datos.csv")
-        ficheroFisico.appendText(data)
+        val dataFile = File(dataFolder, "datos.csv")
+        dataFile.appendText(data)
 
-        val list_of_historic_data = arrayListOf<String>()
-        list_of_historic_data.add(data)
+        val historicalDataList = arrayListOf<String>()
+        historicalDataList.add(data)
 
         //read_colours(folder)
         //read_data_in_storage(folder)
     }
 
-    private fun read_colours(folder: File) {
+    private fun readColours(folder: File) {
         // Creamos la dirección en la que se ubica el archivo
-        val fileName = folder.toString() + "/colores.csv"
+        val fileName = "$folder/colores.csv"
         Log.i("Dirección: ", fileName)
         CSVReader(FileReader(fileName)).use { reader ->
             val r = reader.readAll()
             r.forEach(Consumer { x: Array<String?>? ->
-                System.out.println(Arrays.toString(x))
+                println(Arrays.toString(x))
             })
         }
     }
 
-    private fun read_data_in_storage() {
+    private fun readDataInStorage() {
         val storageDir = getExternalFilesDir("Registros")?.also {
             if (!it.exists()) {
                 it.mkdirs()
@@ -243,71 +239,60 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Se necesitan permisos para ejecutar esta operación!!!
-        File(storageDir.toString(), "registro.csv").forEachLine { Toast.makeText(this, it, Toast.LENGTH_SHORT).show() }
+        File(storageDir.toString(), "registro.csv")
+            .forEachLine { Toast.makeText(this, it, Toast.LENGTH_SHORT).show() }
 
-        val csvReader = CSVReaderBuilder(resources.openRawResource(R.raw.registro).bufferedReader())
-            .withCSVParser(CSVParserBuilder().withSeparator(';').build())
-            .build()
-
-        // Maybe do something with the header if there is one
-        val header = csvReader.readNext()
+//        val csvReader = CSVReaderBuilder(resources.openRawResource(R.raw.registro).bufferedReader())
+//            .withCSVParser(CSVParserBuilder().withSeparator(';').build())
+//            .build()
+//
+//        // Maybe do something with the header if there is one
+//        val header = csvReader.readNext()
 
         //
         var myMap: Map<String, String> = mutableMapOf()
         // Read the rest
-        var line: Array<String>? = csvReader.readNext()
+//        var line: Array<String>? = csvReader.readNext()
 
         val list: List<Array<String>> = ArrayList()
 
         //var line_1: androidx.collection.ArrayMap<String,String> = csvReader.readNext()
 
-        while (line != null) {
-            // Do something with the data
-            println(line[0])
-            println(line[1])
-
-            line = csvReader.readNext()
-        }
+//        while (line != null) {
+//            // Do something with the data
+//            println(line[0])
+//            println(line[1])
+//
+//            line = csvReader.readNext()
+//        }
     }
 
-    private fun check_hangers(): Boolean {
+    //region Validación del input
+    private fun checkHangers(): Boolean {
         return binding.hangersEntry.text.toString().isNotEmpty()
     }
 
-    private fun check_hours(): Boolean {
+    private fun checkHours(): Boolean {
         return (binding.changeStartTimeLabel.text.toString().isNotEmpty() &&
                 binding.colourStartTimeLabel.text.toString().isNotEmpty() &&
                 binding.colourEndTimeLabel.text.toString().isNotEmpty())
     }
 
-    private fun check_colour(): Boolean {
-        var colour_list: MutableList<String> = mutableListOf()
-
-        for (colour in 40100000..40100400) {
-            colour_list.add(colour.toString())
-        }
-        colour_list.add("FIN")
+    private fun checkColour(): Boolean {
+        val colourList = (40100000..40100400).map { it.toString() }.toMutableList()
+        colourList.add("FIN")
 
         return binding.colourEntry.text.toString().isNotEmpty() &&
-                colour_list.contains(binding.colourEntry.text.toString())
+                colourList.contains(binding.colourEntry.text.toString())
     }
+    //endregion Validación del input
 
-    private fun dialog_alert(message: String) {
-        val builder = AlertDialog.Builder(this@MainActivity)
-        // Título.
-        builder.setTitle("ERROR DE REGISTRO")
-
-        // Mensaje a mostrar.
-        builder.setMessage(message)
-
-        // Mostrar un botón neutral para cerrar la alerta.
-        builder.setNeutralButton("ENTENDIDO") { _, _ ->
-        }
-
-        // Creamos la alerta
-        val dialog: AlertDialog = builder.create()
-
-        // Mostramos la alerta.
-        dialog.show()
+    private fun showErrorDialog(message: String) {
+        AlertDialog.Builder(this@MainActivity)
+            .setTitle("ERROR DE REGISTRO")
+            .setMessage(message)
+            .setNeutralButton("ENTENDIDO") { _, _ -> }
+            .create()
+            .show()
     }
 }
